1e4b1f290a2629ab2247210c3b032eba24b833f3
mandariny <mandariny716@gmail.com>
2024-02-02 09:55:25 +0900

    feat: 백엔드 SpringBoot 세팅

---
+++ back/travelplanner/src/test/java/com/ktds/travelplanner/TravelplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktds.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
